3952df7e128ad591841cae15ed8fabc1f4d48221
PreyeO <omusukup@yahoo.com>
2025-04-15 11:47:13 +0100

    changes

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import MenuBar from "@/components/layouts/MenuBar";
 import Footer from "@/components/layouts/Footer";
 import GoogleAnalytics from "@next/third-parties/google";
 import Analytics from "@/analytics/Analytics";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,10 +55,34 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <Analytics />
+        {/* Facebook Pixel Script */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1270192691203133');
+        fbq('track', 'PageView');
+      `}
+        </Script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1270192691203133&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
-        {/* <GoogleAnalytics gtmId="G-7BNBWJMF74" /> */}
+        {/* <iframe height="0" width="0" src="https://www.googletagmanager.com/ns.html?id=G-7BNBWJMF74"></iframe> */}
         <Footer />
         <MenuBar />
       </body>
